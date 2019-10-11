f33cec6576bc4620305684a670e1d8f7d773fb4a
milo0 <milo0@posteo.de>
2019-10-11 13:03:25 +0200

    Redesign directory structure for easier installation

--- install.sh
+++ install.sh
@@ -5,23 +5,5 @@ title() {
     printf "\n${color}$1${nc}\n"
 }
 
-title "Install Ansible"
-sudo apt-get install software-properties-common -y
-sudo apt-add-repository ppa:ansible/ansible -y
-if [ -f /etc/apt/sources.list.d/ansible-ansible-jessie.list ]; then
-    sudo sed -i 's/jessie/trusty/g' /etc/apt/sources.list.d/ansible-ansible-jessie.list
-fi
-sudo apt-get update
-sudo apt-get install curl ansible -y
-
-title "Install viasite-ansible.zsh"
-sudo ansible-galaxy install viasite-ansible.zsh --force
-
-title "Download playbook to /tmp/zsh.yml"
-curl https://raw.githubusercontent.com/viasite-ansible/ansible-role-zsh/master/playbook.yml > /tmp/zsh.yml
-
-title "Provision playbook for root"
-sudo ansible-playbook -i "localhost," -c local /tmp/zsh.yml
-
 title "Provision playbook for $(whoami)"
-sudo ansible-playbook -i "localhost," -c local /tmp/zsh.yml --extra-vars="zsh_user=$(whoami)"
+ansible-playbook -i "localhost," -c local playbook.yml --extra-vars="zsh_user=$(whoami)"
